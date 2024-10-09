053097e34427832e8f14472fa25f4d3fb4fd8576
Alice921029 <145415272+Alice921029@users.noreply.github.com>
2024-10-10 01:33:40 +0800

    Add files via upload

---
+++ project1002App.swift
@@ -0,0 +1,24 @@
+//
+//  project1002App.swift
+//  project1002
+//
+//  Created by Chen Siou on 2024/10/2.
+//
+
+//
+//  project1003App.swift
+//  project1003
+//
+//  Created by Chen Siou on 2024/10/9.
+//
+
+import SwiftUI
+
+@main
+struct project1002App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
